a581c228db56592e48024e089c4f6107c668a333
hyperlogic <hyperlogic@dummyclout.org>
2017-11-19 14:17:23 -0800

    split project_exists and create_new_project_directory functions

--- src/project.py
+++ src/project.py
@@ -7,6 +7,7 @@ class Project:
         self.templates_dir = self.set_template_path()
         self.name = project_name
         self.path = str(self.project_storage_dir + self.name)
+        self.docker_path = str(self.path + "/docker")
         self.project_exists()
 
     # Set path to directory where projects will be stored
@@ -22,11 +23,17 @@ class Project:
     def project_exists(self):
         if os.path.exists(self.path): 
             print("Project exists, continuing...")
+            return True
         else:
-            print("Creating new project directory at {}".format(self.path))
-            os.makedirs(self.path)
+            return False
 
-    
+    def create_new_project_directory():
+        print("Creating new project directory at {}".format(self.path))
+        if os.path.exists(self.project_storage_dir) is False:
+            os.makedirs(self.path)
+        else:
+            os.mkdir(self.path)
+        copytree(self.templates_dir, self.docker_path)
         
 
         
