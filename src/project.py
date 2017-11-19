4f9f53de9b52f9a1e3be46f5a828ee631be8f073
hyperlogic <hyperlogic@dummyclout.org>
2017-11-19 11:48:14 -0800

    Removed uneeded function

--- src/project.py
+++ src/project.py
@@ -2,15 +2,10 @@ class Project:
     def __init__(self, project_name, project_path): 
         self.path = project_path
         self.name = project_name
-    
 
-    def new_project(self):
-        build_project(self.path)
-    
 
     def create_project_dirs(self):
         pass
-    
 
     def build_project(self):
         create_project_dirs()
