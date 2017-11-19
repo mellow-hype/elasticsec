3ca50bc9f3c089941b60a7878d88844d05dd0def
hyperlogic <hyperlogic@dummyclout.org>
2017-11-19 11:48:00 -0800

    Preliminary work on argument handling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 import argparse
+from os import path
  
+def cli_handle_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--new-project", help="Create a new project directory", action="store", dest="project_path")
+    parser.add_argument("type", help="Type of input to be read [default is pcap]", choices=['pcap'], default='pcap')
 
-class CLI:
 
-    def cli_handle_arguments(self):
+    args = parser.parse_args()
+    if args.project_path is not None:
+            print("New project path is: {}".format(args.project_path))
+            exit()
+    else:
         pass
+    
 
 
+
+if __name__ == '__main__':
+
+    cli_handle_arguments()
