ce951cf172dd12067a591b069e8d301cc21a78d7
hyperlogic <hyperlogic@dummyclout.org>
2017-11-19 13:49:05 -0800

    Moved project init stuff to project.py

--- elasticsec.py
+++ elasticsec.py
@@ -5,10 +5,14 @@ from src.project import Project
 def cli_handle_arguments():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("project", help="Target project", action="store")
-    parser.add_argument("type", help="Type of input to be read [default is pcap]", choices=['pcap'], default='pcap')
-    parser.add_argument("input", help="Path to input file to be read")
-
+    parser.add_argument("project", 
+                         help="Target project") 
+    parser.add_argument("type", 
+                         help="Type of input to be read [default is pcap]",
+                         choices=['pcap'])
+    parser.add_argument("input", 
+                         help="Path to input file to be read")
+    
     return parser.parse_args()
 
 
@@ -16,9 +20,5 @@ if __name__ == '__main__':
 
     cli_args = cli_handle_arguments()
     currentProject = Project(cli_args.project)
-
-    if currentProject.project_exists() is True:
-        print("Project exists, continuing...")
-    else:
-        print("Creating new project directory at {}".format(currentProject.path))
+    
     
--- src/project.py
+++ src/project.py
@@ -7,29 +7,24 @@ class Project:
         self.templates_dir = self.set_template_path()
         self.name = project_name
         self.path = str(self.project_storage_dir + self.name)
+        self.project_exists()
 
     # Set path to directory where projects will be stored
     def set_project_storage_path(self):
         return str(os.getcwd()) + '/projects/'
 
-    # Check if path to directory for project storage exists
-    def projects_storage_path_exists(self):
-        if os.path.exists(self.project_storage_dir) and os.path.isdir(os.getcwd() + '/projects/'):
-            return True
-        else:
-            return False
-    
+
     # Set path to directory where project templates are stored
     def set_template_path(self):
         return str(os.getcwd() + '/elk-containers/')
 
-    
     # Check if current project exists
     def project_exists(self):
-        if os.path.exists(self.path):
-            return True
+        if os.path.exists(self.path): 
+            print("Project exists, continuing...")
         else:
-            return False
+            print("Creating new project directory at {}".format(self.path))
+            os.makedirs(self.path)
 
     
         
