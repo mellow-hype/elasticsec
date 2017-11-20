08bf4359021cfe7e9f8c496b0446335ab926b76e
hyperlogic <hyperlogic@dummyclout.org>
2017-11-19 21:11:26 -0800

    Clean up subparsers and add argument handling code for 'new' and 'containers' subcommand

--- elasticsec.py
+++ elasticsec.py
@@ -10,10 +10,11 @@ def cli_handle_arguments():
 
     # Project init subcommand parser
     proj_parser = subparsers.add_parser('new', help='Initialize a new project')
-    proj_parser.add_argument('new_project_name', help='Name of new project')
+    proj_parser.add_argument('projectname', help='Name of new project')
 
     # Input subcommand parser
     input_parser = subparsers.add_parser('input', help='Read an input source')
+    input_parser.add_argument('project', help='Target project')
     input_parser.add_argument('type',choices=['pcap', 'nmap'], 
                                 help='Type of data to be read')
     input_parser.add_argument('input-path', help='Path to input data')
@@ -21,22 +22,39 @@ def cli_handle_arguments():
     # Container subcommand parser
     startup_parser = subparsers.add_parser('containers', help='Manage the Docker containers')
     startup_parser.add_argument('--up', help='Bring the containers up', action='store_true')
-    startup_parser.add_argument('--down', help='Stop the containers', action='store_true')
+    startup_parser.add_argument('--stop', help='Stop the containers', action='store_true')
     startup_parser.add_argument('--restart', help='Restart the containers', action='store_true')
-    startup_parser.add_argument('project-path', help='Path to target project directory')
+    startup_parser.add_argument('project', help='Target project')
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    if args.new_project_name is not None:
-        newProject = Project(args.new_project_name)
-
-        if newProject.project_exists() is False:
-            newProject.create_new_project_directory()
-
-        exit()
 
 
 if __name__ == '__main__':
 
-    cli_handle_arguments()
+    args = cli_handle_arguments()
+
+    # Handle argument to create a new project directory
+    try:
+        if args.projectname is not None:
+            newProject = Project(args.projectname)
+
+            if newProject.project_exists() is False:
+                newProject.create_new_project_directory()
+                exit()
+    except AttributeError:
+        currentProject = Project(args.project)
+
+
+    # Handle container actions
+
+    if args.up is True:
+        currentProject.containers.start()
+    elif args.stop is True:
+        currentProject.containers.stop()
+    elif args.restart is True:
+        currentProject.containers.restart()
+
+    # Handle input functions
+    
 
